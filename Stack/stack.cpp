51dd9fd5784bc7e3c5a8bd22cd927b1678505493
sunny5130 <sunny51305527.com>
2025-01-30 09:09:56 +0530

    push

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -1,48 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class MyStack{
-    public:
+
+class MyStack {
+public:
     int *arr;
     int top;
     int size;
-    MyStack(){
-        arr=new int[100];
-        top=-1;
+
+    MyStack(int capacity = 100) {  
+        size = capacity;
+        arr = new int[size];
+        top = -1;
     }
-void push(int val){
-    if(top==size-1){
-        cout<<"Stack overflow"<<endl;
+
+    void push(int val) {
+        if (top == size - 1) {
+            cout << "Stack overflow" << endl;
+            return;
         }
-    top++;
-    arr[top]=val;
+        arr[++top] = val;
     }
-    void pop(){
-        if(top==-1){
-            cout<<"Stack is empty"<<endl;
+
+    int pop() {
+        if (top == -1) {
+            cout << "Stack is empty" << endl;
             return -1;
         }
-        top--;
-
+        return arr[top--];
     }
 
-    void isEmpty(){
-        if(top==-1){
-            cout<<"Stack is empty";
-        }else{
-            cout<<"Stack is not empty";
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    int peek() {
+        if (top == -1) {
+            cout << "Stack is empty" << endl;
+            return -1;
         }
+        return arr[top];
     }
-
 };
 
-
-int main(){
+int main() {
     MyStack s;
     s.push(1);
     s.push(2);
+    s.push(3);
 
+    cout << "Top element: " << s.peek() << endl;
+    cout << "Popped: " << s.pop() << endl;
+    cout << "Stack empty? " << (s.isEmpty() ? "Yes" : "No") << endl;
 
+    return 0;
 }
 
- input - hello how are you
- output - you are how hello
+
