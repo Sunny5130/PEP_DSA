848ce56d4696da08d73e904b741448a3eee5a38e
sunny5130 <sunny51305527.com>
2025-02-01 09:14:39 +0530

    seond of the day

--- dsa/Stack/tempCodeRunnerFile.cpp
+++ dsa/Stack/tempCodeRunnerFile.cpp
@@ -1,1 +1,24 @@
-// Utility function to print queue
+
+        switch(s[i]){
+            case '+':
+            st.push(oper1 + oper2);
+            break;
+            case '-':
+            st.push(oper1 - oper2);
+            break;
+            case '*':
+            st.push(oper1 * oper2);
+            break;
+            case '/':
+            st.push(oper1 / oper2);
+            break;
+        }
+    }
+    }
+    return st.top();
+}
+
+int main(){
+    cout<<prefix("-+7*45+20")<<endl;
+
+}
