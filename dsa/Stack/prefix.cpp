6158d5c828ce57782da1dace7621986e28452fbf
sunny5130 <sunny51305527.com>
2025-02-01 09:01:38 +0530

    morning code

---
+++ dsa/Stack/prefix.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int prefix(string s){
+    stack<int>st;
+
+    for(int i=s.length()-1;i>=0;i--){
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i]-'0'); //o mei minus integer banane ke liye kiya hai.
+        }else{
+            int oper1=st.top();
+            st.pop();
+            int oper2=st.top();
+            st.pop();
+
+        switch(s[i]){
+            case '+':
+            st.push(oper1 + oper2);
+            break;
+            case '-':
+            st.push(oper1 - oper2);
+            break;
+            case '*':
+            st.push(oper1 * oper2);
+            break;
+            case '/':
+            st.push(oper1 / oper2);
+            break;
+        }
+    }
+    }
+    return st.top();
+}
+
+int main(){
+    cout<<prefix("-+7*45+20")<<endl;
+
+}
