b3de6a4659e7017bb37958d65a681d7d764671da
sunny5130 <sunny51305527.com>
2025-07-29 15:44:45 +0530

    tree

---
+++ Tree/tree.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+    static int ind=-1;
+    Node* buildtree(vector<int>arr){
+        ind++;
+        if(arr[ind]==-1){
+            return NULL;
+        }
+        Node * root=new Node(arr[ind]);
+        root->left=buildtree(arr);
+        root->right=buildtree(arr);
+        return root;
+    }
+int main(){
+    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=buildtree(preorder);
+    cout<<root->data<<endl;
+    cout<<root->left->data<<endl;
+    cout<<root->right->data<<endl;
+    return 0;
+}
